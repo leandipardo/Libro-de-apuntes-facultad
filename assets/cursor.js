a8e2a46eee2146c6eead9744fe84bf9315579f3d
leandipardo <leandipardo@gmail.com>
2024-09-16 03:26:59 -0300

    estilazos

---
+++ assets/cursor.js
@@ -0,0 +1,29 @@
+const cursor = document.getElementById("customCursor"),
+  pointer = document.querySelectorAll("[pointer]");
+
+export default function cursorStyles() {
+  // Selecciona el elemento del cursor
+  // Función para mover el cursor con el ratón
+  document.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.pageX + "px";
+    cursor.style.top = e.pageY + "px";
+  });
+
+  //animacion de salida y entrada ventana
+  document.addEventListener("mouseleave", (e) => {
+    cursor.classList.add("custom-cursor-dissapear");
+  });
+  document.addEventListener("mouseenter", (e) => {
+    cursor.classList.remove("custom-cursor-dissapear");
+  });
+
+  //animacion de pointer
+  pointer.forEach((e) => {
+    e.addEventListener("mouseenter", (e) => {
+      cursor.classList.add("pointer-animation");
+    });
+    e.addEventListener("mouseleave", (e) => {
+      cursor.classList.remove("pointer-animation");
+    });
+  });
+}
--- assets/modules.js
+++ assets/modules.js
@@ -1,3 +1,4 @@
+import cursorStyles from "./cursor.js";
 import fetchStart from "./fetch-html.js";
 import focus from "./focus.js";
 import rotarHoja from "./rotar-hoja.js";
@@ -8,12 +9,5 @@ document.addEventListener("DOMContentLoaded", (e) => {
   focus();
   rotarHoja("#booklet-rotate");
   seleccionarSemestre();
-});
-// Selecciona el elemento del cursor
-const cursor = document.getElementById("customCursor");
-
-// Función para mover el cursor con el ratón
-document.addEventListener("mousemove", (e) => {
-  cursor.style.left = e.pageX + "px";
-  cursor.style.top = e.pageY + "px";
+  cursorStyles();
 });
